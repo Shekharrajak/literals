5a1f910854cb02ef4f6e5f21d24eaa71f64062f9
Yuki KAN <re@pixely.jp>
2014-03-17 05:20:41 +0900

    update

--- literals/js/editor.js
+++ literals/js/editor.js
@@ -2,11 +2,18 @@
 (function () {
   "use strict";
 
+  var app = WinJS.Application;
+  var Activation = Windows.ApplicationModel.Activation;
+  var AccessList = Windows.Storage.AccessCache.StorageApplicationPermissions.futureAccessList;
+  var FileIO = Windows.Storage.FileIO;
+
   var literals = {
     view: {},
     stat: {},
     f: {},
-    config: {}
+    def: {},
+    config: {},
+    sessions: []
   };
 
   //
@@ -26,83 +33,198 @@
 
   // .txt config
   literals.config.txt = flagrate.extendObject({}, literals.config.global);
-  literals.config.txt.aceMode = 'plain_text';
+  literals.config.txt.mode = 'plain_text';
   literals.config.txt.useTabChar = true;
   literals.config.txt.styleActiveLine = false;
+
+
+  // .xml config
+  literals.config.xml = flagrate.extendObject({}, literals.config.global);
+  literals.config.xml.mode = 'xml';
+
+  // .svg config
+  literals.config.svg = flagrate.extendObject({}, literals.config.global);
+  literals.config.svg.mode = 'svg';
 
   // .html config
   literals.config.html = flagrate.extendObject({}, literals.config.global);
   literals.config.htm = literals.config.html;// .htm alias
-  literals.config.html.aceMode = 'html';
+  literals.config.html.mode = 'html';
 
   // .css config
   literals.config.css = flagrate.extendObject({}, literals.config.global);
-  literals.config.css.aceMode = 'css';
+  literals.config.css.mode = 'css';
 
   // .less config
   literals.config.less = flagrate.extendObject({}, literals.config.global);
-  literals.config.less.aceMode = 'less';
+  literals.config.less.mode = 'less';
+
+  // .sass config
+  literals.config.sass = flagrate.extendObject({}, literals.config.global);
+  literals.config.sass.mode = 'sass';
+
+  // .scss config
+  literals.config.scss = flagrate.extendObject({}, literals.config.global);
+  literals.config.scss.mode = 'scss';
 
   // .js config
   literals.config.js = flagrate.extendObject({}, literals.config.global);
-  literals.config.js.aceMode = 'javascript';
+  literals.config.js.mode = 'javascript';
 
   // .ts config
   literals.config.ts = flagrate.extendObject({}, literals.config.global);
-  literals.config.ts.aceMode = 'typescript';
+  literals.config.ts.mode = 'typescript';
 
-  var app = WinJS.Application;
-  var activation = Windows.ApplicationModel.Activation;
+  // .jsx config
+  literals.config.jsx = flagrate.extendObject({}, literals.config.global);
+  literals.config.jsx.mode = 'jsx';
 
+  // .hx config
+  literals.config.hx = flagrate.extendObject({}, literals.config.global);
+  literals.config.hxml = literals.config.hx;// .hxml alias
+  literals.config.hx.mode = 'haxe';
+
+  // .coffee config
+  literals.config.coffee = flagrate.extendObject({}, literals.config.global);
+  literals.config.coffee.mode = 'coffee';
+
+  // .as config
+  literals.config.as = flagrate.extendObject({}, literals.config.global);
+  literals.config.as.mode = 'actionscript';
+
+  // .json config
+  literals.config.json = flagrate.extendObject({}, literals.config.global);
+  literals.config.json.mode = 'json';
+
+  //
+  // functions
+  //
+
+  // init
   literals.f.init = function () {
     literals.view.body = document.getElementById('body');
 
     // editor set
     literals.view.editorSet = flagrate.createElement('div', { 'class': 'editor-set' });
-    literals.view.body.appendChild(literals.view.editorSet);
+    literals.view.editorSet.insertTo(literals.view.body);
 
     // editor container
     literals.view.editorContainer = flagrate.createElement('div', { 'class': 'editor-container' });
-    literals.view.editorSet.appendChild(literals.view.editorContainer);
+    literals.view.editorContainer.insertTo(literals.view.editorSet);
 
     // editor status bar
     literals.view.statusBar = flagrate.createElement('div', { 'class': 'editor-status-bar' });
-    literals.view.editorSet.appendChild(literals.view.statusBar);
+    literals.view.statusBar.insertTo(literals.view.editorSet);
 
-    // test
-    literals.stat.mode = 'text';
-    literals.view.editor = CodeMirror(literals.view.editorContainer, {
-      lineNumbers: true,
-      value: 'hoge'
-    });
-
+    // TODO:
     literals.view.statusBar.insert('<ul><li>test</li><li>test</li><li>test</li></ul>');
   };
 
-  app.onactivated = function (a) {
-    if (a.detail.kind === activation.ActivationKind.launch) {
+  // create session
+  literals.f.createSession = function () {
+    var session = {
+      name: null,
+      type: 'txt',
+      content: '',
+      file: {}
+    };
+
+    literals.sessions.push(session);
+
+    return session;
+  };
+
+  // create session by file
+  literals.f.createSessionByFile = function (file) {
+    // TODO: check if a file is already open.
+
+    var session = {
+      name: file.name,
+      type: file.fileType.toLowerCase(),
+      content: null,
+      file: {
+        path: file.path,
+        token: AccessList.add(file)
+      }
+    };
+
+    literals.sessions.push(session);
+
+    return session;
+  };
+
+  // select session
+  literals.f.selectSession = function (session) {
+    literals.stat.session = session;
+
+    if (session.content === null && typeof session.file.token === 'string') {
+      AccessList.getFileAsync(session.file.token).done(function (file) {
+        FileIO.readTextAsync(file).done(function (content) {
+          session.content = content;
+          literals.f.initSession();
+        });
+      });
+    } else {
+      literals.f.initSession();
+    }
+
+    return session;
+  };
+
+  // init session
+  literals.f.initSession = function () {
+    // clear editor
+    literals.view.editor = null;
+    literals.view.editorContainer.update();
+
+    // create editor
+    literals.view.editor = CodeMirror(literals.view.editorContainer, {
+      lineNumbers: true,
+      value: literals.stat.session.content
+    });
+  };
+
+  //
+  // init
+  //
+
+  // activated
+  app.onactivated = function (e) {
+    if (e.detail.kind === Activation.ActivationKind.launch) {
       literals.f.init();
 
-      if (a.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-        // TODO: このアプリケーションは新しく起動しました。ここでアプリケーションを
-        // 初期化します。
+      if (e.detail.previousExecutionState !== Activation.ApplicationExecutionState.terminated) {
+        // TODO: newly started.
       } else {
-        // TODO: このアプリケーションは中断状態から再度アクティブ化されました。
-        // ここでアプリケーションの状態を復元します。
+        // TODO: resume.
       }
 
-      a.setPromise(WinJS.UI.processAll());
+      //e.setPromise(WinJS.UI.processAll());
+    }
+
+    if (e.detail.kind === Activation.ActivationKind.file) {
+      var files = e.detail.files;
+
+      // create sessions
+      files.forEach(function (file) {
+        literals.f.createSessionByFile(file);
+      });
+
+      // select latest session
+      literals.f.selectSession(literals.sessions[literals.sessions.length - 1]);
+    } else {
+      if (literals.sessions.length === 0) {
+        literals.f.selectSession(literals.f.createSession());
+      }
     }
   };
 
-  app.oncheckpoint = function (args) {
-    // TODO: このアプリケーションは中断しようとしています。ここで中断中に
-    // 維持する必要のある状態を保存します。中断中に自動的に保存され、
-    // 復元される WinJS.Application.sessionState オブジェクトを使用
-    // できます。アプリケーションを中断する前に
-    // 非同期操作を完了する必要がある場合は、
-    // args.setPromise() を呼び出してください。
+  // checkpoint
+  app.oncheckpoint = function (e) {
+    // TODO: terminating. use WinJS.Application.sessionState object.
+    // if async operation needed, call the e.setPromise() method.
   };
 
+  // start
   app.start();
-})();
+}());
